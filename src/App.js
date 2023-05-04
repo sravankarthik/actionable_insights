cfec4101c303c5a41945ddbba6ad51b3f355862b
Sravan Karthik <77857519+sravankarthik@users.noreply.github.com>
2023-05-05 02:43:39 +0530

    eigth

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Entity from './components/Entity';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav_bar from './components/Nav-bar';
 import Search from './components/Search';
 import Issues from './components/Issues';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Container } from "react-bootstrap";
 
-function App() {
+let App = () => {
+  let [status, setStatus] = useState(false);
+  let [details, setDetails] = useState();
+  let [items, setItems] = useState([]);
+  let [package_, setPackage] = useState();
+
+  let handlesubmit = (e) => {
+    // e.preventDefault();
+    // console.log(package_);
+    axios.get(`http://127.0.0.1:5000/scores?app=${package_}`).then((response) => {
+      setDetails(response);
+      axios.get(`http://127.0.0.1:5000/suggestions?app=${package_}`).then((response) => {
+        setItems(response);
+        setStatus(true)
+      })
+    })
+  }
   return (
     <div >
       <Nav_bar />
-      <Search />
-      <Entity details={{
-        "app": 0.3,
-        "coupon": 0.2,
-        "cuisine": 0.3,
-        "customer support": -0.5,
-        "delivery": 0.6,
-        "live tracking": -0.5,
-        "location": -0.3,
-        "ordering": 0.4,
-        "promo code": 0.4,
-        "refund": -0.3,
-        "restaurant": -0.1,
-        "user interface": 0.4,
-        "zomato": 0.1,
-        "zomato gold": -0.3
-      }} />
-      <Issues />
+      <Container className="m-4">
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            onChange={(e) => setPackage(e.target.value)}
+          />
+          <Button variant="outline-success" onClick={(e) => handlesubmit(e)}>Search</Button>
+        </Form>
+      </Container>
+
+      {status && <Entity details={details} />}
+      {status && <Issues items={items} />}
     </div>
   );
 }
