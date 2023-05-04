3030deb0f8853e45028d9196b8a167b8aab2e5a2
Sravan Karthik <77857519+sravankarthik@users.noreply.github.com>
2023-05-04 21:29:40 +0530

    second

--- src/components/Entity.js
+++ src/components/Entity.js
@@ -14,9 +14,8 @@ const Entity = ({ details }) => {
                             <Card.Title>{key}</Card.Title>
                             <Card.Text>
                                 {details[key]}
-                                {details["progress-bar"] = (((details[key] + 1) / 2) * 100) >= 50 ? "progress-bar bg-success" : "prgress-bar bg-danger"}
                                 <div className="progress">
-                                    <div className={details["progress-bar"]} role="progressbar" style={{ width: ((details[key] + 1) / 2) * 100 }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div className={(((details[key] + 1) / 2) * 100) >= 50 ? "progress-bar bg-success" : "prgress-bar bg-danger"} role="progressbar" style={{ width: ((details[key] + 1) / 2) * 100 }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
 
                             </Card.Text>
